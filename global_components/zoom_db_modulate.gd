8b3df8f4450e8d173aaa4d2579d8def1f77edc13
Kai <kai@kana.jetzt>
2024-10-16 21:48:43 +0200

    feat: :sparkles: that's a lot of audio

---
+++ global_components/zoom_db_modulate.gd
@@ -0,0 +1,19 @@
+extends Node
+
+
+@export var audio_player: Node
+@export var zoom_db_curve: Curve
+
+
+func _ready() -> void:
+	if audio_player == null or zoom_db_curve == null:
+		assert(false, "Make sure to set audio_player and zoom_db_curve")
+	if not audio_player is AudioStreamPlayer and not audio_player is AudioStreamPlayer2D:
+		assert(false, "Only AudioStreamPlayer and AudioStreamPlayer2D are supported as audio_player.")
+
+
+func _process(delta: float) -> void:
+	var camera := get_viewport().get_camera_2d()
+
+	var t = camera.zoom.x / Global.zoom_in_max.x
+	audio_player.volume_db = zoom_db_curve.sample_baked(t)
